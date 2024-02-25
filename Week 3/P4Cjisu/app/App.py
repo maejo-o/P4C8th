853bb26ed64a99a0790e6cd940beebd2d84fdb37
maejo-o <efforterjisulee@gmail.com>
2024-02-25 09:03:04 +0900

    week 3 con

--- Week 3/P4Cjisu/app/App.py
+++ Week 3/P4Cjisu/app/App.py
@@ -25,28 +25,30 @@ def index():
 def add():
     if request.method == 'POST':
         title = request.form['title']
-        content = request.form['context']  # 'maintext' -> 'context'
+        content = request.form['context']  
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO board (title, maintext) VALUES (%s, %s)", (title, content))
         mysql.connection.commit()
         cur.close()
         return redirect(url_for('index'))
 
-@app.route('/update/<int:no>', methods=['GET', 'POST'])  # 'uid' -> 'no'
+@app.route('/update/<int:no>', methods=['GET', 'POST'])
 def update(no):
     cur = mysql.connection.cursor()
     if request.method == 'POST':
         title = request.form['title']
-        content = request.form['context']  # 'maintext' -> 'context'
+        content = request.form['context']  # 'context' 필드명으로 수정
         cur.execute("UPDATE board SET title = %s, maintext = %s WHERE no = %s", (title, content, no))
         mysql.connection.commit()
+        cur.close()
         return redirect(url_for('index'))
     else:
         cur.execute("SELECT * FROM board WHERE no = %s", [no])
         post = cur.fetchone()
+        cur.close()
         return render_template('update.html', post=post)
 
-@app.route('/delete/<int:no>', methods=['POST'])  # 'uid' -> 'no'
+@app.route('/delete/<int:no>', methods=['POST'])  
 def delete(no):
     cur = mysql.connection.cursor()
     cur.execute("DELETE FROM board WHERE no = %s", [no])
