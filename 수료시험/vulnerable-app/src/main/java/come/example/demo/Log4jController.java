e17a22761c1a07b766cf8ffac2b92d630c78d52c
maejo-o <efforterjisulee@gmail.com>
2024-04-28 03:29:59 +0900

    testtest

---
+++ 수료시험/vulnerable-app/src/main/java/come/example/demo/Log4jController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Log4jController {
+
+    private static final Logger logger = LogManager.getLogger(Log4jController.class);
+
+    @GetMapping("/log")
+    @ResponseBody
+    public String logInput(@RequestParam String input) {
+        logger.info("User input: {}", input);
+        return "Logged: " + input;
+    }
+}
