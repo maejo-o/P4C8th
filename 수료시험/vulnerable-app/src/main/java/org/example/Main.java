4350cbedc8be407398dae464701394afa36e477e
maejo-o <efforterjisulee@gmail.com>
2024-04-28 14:36:18 +0900

    last commmmmmit

--- 수료시험/vulnerable-app/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import com.example.demo.Log4jController;
-
-public class Main {
-    public static void main(String[] args) {
-        Log4jController log4jController = new Log4jController();
-        log4jController.executeLogging();
-    }
-}
